bb66bf299b656fbece14a66a5e8ceb37eecab03f
Martin Lowe <martinlowe673@gmail.com>
2024-12-23 17:03:41 +0000

    text

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,21 @@
+import images from '@/constants/images';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView ,Image} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignIn = () =>{
     return(
-        <View>
-            <Text>Sign In</Text>
-        </View>
+        <SafeAreaView className='bg-white h-full'>
+           <ScrollView contentContainerClassName='h-full'>
+            <Image  source={images.onboarding} className="w-full h-4/6" resizeMode="contain"/>
+                <View className='px-10'>
+                    <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Restate</Text>
+                    <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>Lets get you close too {"\n"}
+                        <Text>Your ideal home</Text>
+                    </Text>
+                </View>
+           </ScrollView>
+        </SafeAreaView>
     )
 }
 
