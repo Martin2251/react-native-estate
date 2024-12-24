d0fa51677b75c98edd2618e7fddbca97993ff8a9
Martin Lowe <martinlowe673@gmail.com>
2024-12-24 08:17:38 +0000

    interfaces

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -1,4 +1,18 @@
 import { createContext } from "react";
 
+interface User{
+    $id: string;
+    name:string;
+    email: string;
+    avatar:string
+}
+
+interface GlobalContextType{
+    isLoggedIn: boolean;
+    user: User | null;
+    loading:boolean;
+    refetch:(newParams ? :Record<string, string | number>) => Promise<void>
+}
+
 const GlobalContext =  createContext<GlobalContextType | undefined>
 
